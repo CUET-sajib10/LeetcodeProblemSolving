e9e1dd473ecd8a710e51b31b541a0c866c88388d
Sajib Saif <sajibcse10@gmail.com>
2021-11-02 22:26:37 +0600

    Leetcode 78: Subsets

---
+++ LeetcodeProblemSolving/leetcodejava/src/main/java/com/sajib/leetcodejava/MyClass.java
@@ -0,0 +1,8 @@
+package com.sajib.leetcodejava;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ LeetcodeProblemSolving/leetcodejava/src/main/java/com/sajib/leetcodejava/Subset78.java
@@ -0,0 +1,42 @@
+package com.sajib.leetcodejava;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Subset78 {
+
+    static List<List<Integer>> resultedList = new ArrayList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+
+        return null;
+    }
+
+
+    static void generateSubset(List<Integer>result, int index,int [] nums){
+
+        if(index<0){
+//            for (int num: result) {
+//                System.out.print(num);
+//            }
+//            System.out.println("");
+            resultedList.add(result);
+            return;
+        }
+
+        List<Integer> tempList = new ArrayList<>();
+        tempList.addAll(result);
+        tempList.add(nums[index]);
+        generateSubset(tempList,index-1,nums);
+
+        generateSubset(result,index-1,nums);
+    }
+
+    public static void main(String[] args) {
+
+        int[] input = new int[]{3, 2, 1};
+        generateSubset(new ArrayList<Integer>(),input.length-1,input);
+
+    }
+}
