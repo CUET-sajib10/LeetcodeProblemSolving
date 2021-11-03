558db32d0f1d4aec239f936b7fa672984bcda08c
Sajib Saif <sajibcse10@gmail.com>
2021-11-03 11:44:37 +0600

    Leetcode 39: Combination sum

---
+++ LeetcodeProblemSolving/leetcodejava/src/main/java/com/sajib/leetcodejava/CombinationSum39.java
@@ -0,0 +1,45 @@
+package com.sajib.leetcodejava;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum39 {
+
+
+    static List<List<Integer>> resultedList = new ArrayList<List<Integer>>();
+
+    static void printList(List<Integer> list){
+        for (int item: list) {
+            System.out.print(item);
+        }
+        System.out.println("");
+
+    }
+
+    static void generateCombinationSum(List<Integer> combination ,int sum, int[] candidates, int target, int previousItem){
+
+        if(sum> target) return;
+
+        if(sum == target){
+           // printList(combination);
+            resultedList.add(combination);
+            return;
+        }
+
+        for(int item :candidates){
+
+            if(item>= previousItem) {
+                List<Integer> tempList = new ArrayList<>();
+                tempList.addAll(combination);
+                tempList.add(item);
+                generateCombinationSum(tempList, sum + item, candidates, target, item);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] input = new int[]{2, 3, 6,7};
+        generateCombinationSum(new ArrayList<Integer>(),0,input,7,Integer.MIN_VALUE);
+    }
+
+}
