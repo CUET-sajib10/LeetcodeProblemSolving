f6d94adaf27469e5bc11133a7097cf65373e6629
Sajib Saif <sajibcse10@gmail.com>
2021-11-08 16:18:45 +0600

    Leetcode 509: Fibonocci number

---
+++ LeetcodeProblemSolving/leetcodejava/src/main/java/com/sajib/leetcodejava/tried/FibonocciNumber509.java
@@ -0,0 +1,38 @@
+package com.sajib.leetcodejava.tried;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class FibonocciNumber509 {
+
+
+
+
+    public static void main(String[] args) {
+
+        int n = 6;
+        dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        int ans = fibonocciNumber(n);
+        System.out.println(ans);
+
+    }
+    private static int[] dp;
+    private static int fibonocciNumber(int n) {
+
+        if(n==0) return 0;
+        if(n == 1 || n == 2) return 1;
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        int a = fibonocciNumber(n-1);
+        int b = fibonocciNumber(n-2);
+
+        int sum = a+b;
+        dp[n] = sum;
+
+        return sum;
+    }
+}
